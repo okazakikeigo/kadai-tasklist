7bf17952ba88e16e7d85c9e533fa52ea8252b185
kanaria <kanaria@techacademy.jp>
2019-07-23 05:36:21 +0000

    DRY

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_17_091046) do
+ActiveRecord::Schema.define(version: 2019_07_22_143323) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
